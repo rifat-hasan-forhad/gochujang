6634058ade3afcfadcb571fa3add39709bb18f15
Rifat Hasan Forhad <forhadrifat200@gmail.com>
2026-03-27 21:43:31 +0600

    adding page-2

--- script.js
+++ script.js
@@ -12,3 +12,63 @@ const navAnim = () => {
 };
 
 navAnim();
+
+const page2Anim = () => {
+  const p2Img = document.querySelector(".p2-img");
+  const p2RightElem1 = document.querySelector("#p2-right-elem1");
+  const p2RightElem2 = document.querySelector("#p2-right-elem2");
+  const p2RightElem3 = document.querySelector("#p2-right-elem3");
+  const p2RightElem4 = document.querySelector("#p2-right-elem4");
+
+  const mouseMove = (e) => {
+    p2Img.style.opacity = 1;
+    p2Img.style.left = e.x + "px";
+    p2Img.style.top = e.y + "px";
+  };
+
+  const mouseLeave = () => {
+    p2Img.style.opacity = 0;
+  };
+
+  p2RightElem1.addEventListener("mousemove", (e) => {
+    p2Img.src = "images & videos/page2-img1.webp";
+    p2Img.style.borderRadius = "50%";
+    mouseMove(e);
+  });
+
+  p2RightElem1.addEventListener("mouseleave", () => {
+    mouseLeave();
+  });
+
+  p2RightElem2.addEventListener("mousemove", (e) => {
+    p2Img.src = "images & videos/page2-img2.jpg";
+    p2Img.style.borderRadius = "30% 30% 80% 80%";
+    mouseMove(e);
+  });
+
+  p2RightElem2.addEventListener("mouseleave", () => {
+    mouseLeave();
+  });
+
+  p2RightElem3.addEventListener("mousemove", (e) => {
+    p2Img.src = "images & videos/page2-img3.jpg";
+    p2Img.style.borderRadius = "60px 70px 40px 140px";
+    mouseMove(e);
+  });
+
+  p2RightElem3.addEventListener("mouseleave", () => {
+    mouseLeave();
+  });
+
+  p2RightElem4.addEventListener("mousemove", (e) => {
+    p2Img.src = "images & videos/page2-img4.jpg";
+    p2Img.style.borderRadius = "10px 40px 5px 5px";
+    mouseMove(e);
+  });
+
+  p2RightElem4.addEventListener("mouseleave", () => {
+    mouseLeave();
+  });
+};
+
+page2Anim();
